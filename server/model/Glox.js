f36210dcd47c5c819ded88dda1cb910d6f21c4cd
SaudurRahman997 <muhammadsaudurrahman@gmail.com>
2025-08-13 22:32:38 +0500

    Front-end added

--- server/model/Glox.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const planetSchema = new Schema({
-    galaxy: { type: String, required: true },
-    text: { type: String, required: true },
-});
-
-module.exports = mongoose.model('Glox', planetSchema);
